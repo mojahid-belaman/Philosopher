d47105a974cdffa83d0c5387897171840a9932f0
Mojahid Belaman <mbelaman@e2r3p5.1337.ma>
2021-05-10 16:06:50 +0000

    philosopher segfault

--- philo_one/main.c
+++ philo_one/main.c
@@ -1,19 +1,137 @@
 #include "philo_one.h"
 
+unsigned	long	get_time()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+void	aff_msg(t_var *var, int status)
+{
+	pthread_mutex_lock(&var->msg_mutex);
+	ft_putnbr_fd(get_time() - var->t_start, 1);
+	ft_putstr_fd(" ", 1);
+	ft_putnbr_fd(var->index + 1, 1);
+	ft_putstr_fd(" ", 1);
+	if (status == FORK)
+		ft_putstr_fd("has taken a fork\n", 1);
+	else if (status == EAT)
+		ft_putstr_fd("is eating\n", 1);
+	else if (status == SLEEP)
+		ft_putstr_fd("is sleeping\n", 1);
+	else if (status == THINK)
+		ft_putstr_fd("is thinking\n", 1);
+	if (status != DIED)
+		pthread_mutex_unlock(&var->msg_mutex);
+}
+
+int	take_forks(t_var *var)
+{
+	if (var->is_died)
+	{
+		pthread_mutex_lock(&var->forks_mutex[var->ph_left]);
+		aff_msg(var, FORK);
+		if (var->is_died)
+		{
+			pthread_mutex_lock(&var->forks_mutex[var->ph_right]);
+			aff_msg(var, FORK);
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int	eat_phil(t_var *var)
+{
+	int	t_eat;
+
+	t_eat = var->time_eat * 1000;
+	if (var->is_died)
+	{
+		aff_msg(var, EAT);
+		usleep(t_eat);
+		return (1);
+	}
+	return (0);
+}
+
+int	release_forks(t_var *var)
+{
+	if (var->is_died)
+	{
+		pthread_mutex_unlock(&var->forks_mutex[var->ph_left]);
+		pthread_mutex_unlock(&var->forks_mutex[var->ph_right]);
+		return (1);
+	}
+	return (0);
+}
+
+int	sleep_phil(t_var *var)
+{
+	int t_sleep;
+
+	t_sleep = var->time_sleep * 1000;
+	if (var->is_died)
+	{
+		aff_msg(var, SLEEP);
+		usleep(t_sleep);
+		return (1);
+	}
+	return (0);
+}
+
+void	*routine_phil(void	*data)
+{
+	t_var *var;
+	
+	var = data;
+	while (var->is_died)
+	{
+		if (!take_forks(var))
+			return (NULL);
+		if (!eat_phil(var))
+			return (NULL);
+		if (!release_forks(var))
+			return (NULL);
+		if (!sleep_phil(var))
+			return (NULL);
+		aff_msg(var, THINK);
+		usleep(100);
+	}
+	
+	return (NULL);
+}
+
 void	phil_init(t_var *var)
 {
 	int	i;
 
 	i = -1;
-	var->phil = malloc(sizeof(t_phil) * var->num_phil);
+	//initial mutex the each philosopher
+	var->forks_mutex = malloc(sizeof(pthread_mutex_t) * var->num_phil);
+	pthread_mutex_init(&var->msg_mutex, NULL);
+	while (++i < var->num_phil)
+		pthread_mutex_init(&var->forks_mutex[i], NULL);
+	i = -1;
 	while (++i < var->num_phil)
 	{
-		var->phil[i].index = i;
-		var->phil[i].eat_count = -1;
-		var->phil[i].ph_left = i;
-		var->phil[i].ph_right = (i + 1) % var->num_phil;
+		var[i].index = i;
+		var[i].eat_count = 0;
+		var[i].ph_left = i;
+		var[i].ph_right = (i + 1) % var->num_phil;
 	}
-	
+	var->is_died = 1;
+
+	//start circle philosopher
+	var->t_start =  get_time();
+	i = -1;
+	while (++i < var->num_phil)
+	{
+		pthread_create(&var[i].tid, NULL, routine_phil, &var[i]);
+		pthread_detach(var[i].tid);
+		usleep(1000);
+	}
 }
 
 int main(int argc, char **argv)
@@ -43,8 +161,10 @@ int main(int argc, char **argv)
 		var.time_die = ft_atoi(argv[2]);
 		var.time_eat = ft_atoi(argv[3]);
 		var.time_sleep = ft_atoi(argv[4]);
-		phil_init(&var);		
+		if (argc == 6)
+			var.count = ft_atoi(argv[5]);
+		//initial each philosopher
+		phil_init(&var);
 	}
-	
 	return (0);
 }
--- philo_one/philo_one.h
+++ philo_one/philo_one.h
@@ -5,27 +5,30 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-typedef struct s_phil
+#include <sys/time.h>
+# define EAT 1
+# define SLEEP 2
+# define THINK 3
+# define FORK 4
+# define DIED 5
+typedef struct s_var
 {
+    int 	        num_phil;
+    int 	        time_die;
+    int 	        time_eat;
+    int 	        time_sleep;
+	int		        is_died;
+    int             count;
+    unsigned    long    t_start;
+    unsigned    long    limit;
     int 		    index;
     int 		    ph_eat;
     int 		    ph_left;
     int			    ph_right;
     int			    eat_count;
     pthread_t       tid;
-	pthread_mutex_t	mutex;
-
-}              t_phil;
-
-typedef struct s_var
-{
-    int 	num_phil;
-    int 	time_die;
-    int 	time_eat;
-    int 	time_sleep;
-	int		some_die;
-	t_phil	*phil;
+    pthread_mutex_t *forks_mutex;
+    pthread_mutex_t msg_mutex;
 
 }              t_var;
 
@@ -33,4 +36,6 @@ long long   ft_atoi(const char *str);
 void	    ft_putstr_fd(char *s, int fd);
 size_t	    ft_strlen(const char *str);
 int         ft_isnum(int c);
+void        ft_putchar_fd(char c, int fd);
+void        ft_putnbr_fd(int n, int fd);
 #endif
